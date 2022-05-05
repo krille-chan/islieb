09661e2a457f1244734b5553c87f9ada2aba52c3
Christian Pauly <christian-pauly@posteo.de>
2022-05-05 08:10:58 +0200

    feat: Add firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:islieb/model/islieb_reader.dart';
 import 'package:islieb/widgets/islieb_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   final isliebReader = await IsliebReader.init();
   runApp(IsliebApp(isliebReader: isliebReader));
 }
